f476a39fbbaa15fa791de8a2df6a5d729f36c94f
Omar <o.allaham@icloud.com>
2022-08-17 16:44:36 +0200

    added test helper functions

---
+++ Tests/Solid PrincipleTests/Helper/SharedTestHelpers.swift.swift
@@ -0,0 +1,9 @@
+//
+//  Created by Omar Allaham on 8/17/22.
+//
+
+import XCTest
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
---
+++ Tests/Solid PrincipleTests/Helper/XCTestCase+MemoryLeakTracking.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Omar Allaham on 8/17/22.
+//
+
+import XCTest
+
+extension XCTestCase {
+    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
+    }
+}
